44f2db1ed095fe830921c7206d6464020b631a2a
ianmcvann <ian.mcvann@gmail.com>
2025-04-09 17:24:52 -0400

    Add triggers and views.

---
+++ StudentDB_Framework/Triggers.sql
@@ -0,0 +1,59 @@
+﻿CREATE TRIGGER limit_course_enrollment
+ON Taking
+AFTER INSERT
+AS
+BEGIN
+    IF EXISTS (
+         SELECT 1
+         FROM (
+             SELECT t.GWID, c.semester, COUNT(*) AS course_count
+             FROM Taking t
+             INNER JOIN Courses c ON t.CRN = c.CRN
+             WHERE t.GWID IN (SELECT DISTINCT GWID FROM inserted)
+             GROUP BY t.GWID, c.semester
+         ) AS Enrollment
+         WHERE Enrollment.course_count > 6
+    )
+    BEGIN
+        ROLLBACK TRANSACTION;
+        THROW 50000, 'Student cannot enroll in more than 6 courses per semester', 1;
+    END
+END;
+GO
+CREATE TRIGGER limit_scholarships
+ON Has
+AFTER INSERT
+AS
+BEGIN
+    IF EXISTS (
+         SELECT 1
+         FROM (
+             SELECT GWID, COUNT(*) AS scholarship_count
+             FROM Has
+             WHERE GWID IN (SELECT DISTINCT GWID FROM inserted)
+             GROUP BY GWID
+         ) AS SCounts
+         WHERE SCounts.scholarship_count > 3  -- set your desired limit here
+    )
+    BEGIN
+         ROLLBACK TRANSACTION;
+         THROW 50001, 'Student cannot have more than 2 scholarships', 1;
+    END
+END;
+GO
+CREATE TRIGGER stop_course_self_requirement
+ON Courses
+AFTER INSERT, UPDATE
+AS
+BEGIN
+    IF EXISTS (
+         SELECT 1
+         FROM inserted
+         WHERE requires IS NOT NULL 
+           AND requires = CAST(CRN AS VARCHAR(255))
+    )
+    BEGIN
+         ROLLBACK TRANSACTION;
+         THROW 50003, 'A course cannot require itself.', 1;
+    END
+END;
