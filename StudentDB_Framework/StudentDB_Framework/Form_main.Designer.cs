784a1be7c155af9638ae667dd10b9905ba0f2e42
buneabt <buneabt@FB-TOMP405-L37>
2025-04-10 00:09:55 -0400

    Functionality To Main

--- StudentDB_Framework/StudentDB_Framework/Form_main.Designer.cs
+++ StudentDB_Framework/StudentDB_Framework/Form_main.Designer.cs
@@ -31,25 +31,18 @@ namespace StudentDB_Framework
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
             this.button_logout = new System.Windows.Forms.Button();
             this.textBox1 = new System.Windows.Forms.TextBox();
-            this.studentDBDataSet = new StudentDB_Framework.StudentDBDataSet();
-            this.studentsBindingSource = new System.Windows.Forms.BindingSource(this.components);
-            this.studentsTableAdapter = new StudentDB_Framework.StudentDBDataSetTableAdapters.StudentsTableAdapter();
+            this.label1 = new System.Windows.Forms.Label();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
-            this.studentDBDataSetBindingSource = new System.Windows.Forms.BindingSource(this.components);
-            this.studentDBDataSetBindingSource1 = new System.Windows.Forms.BindingSource(this.components);
-            this.gwidDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.emailDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.firstDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.lastDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.genderDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            ((System.ComponentModel.ISupportInitialize)(this.studentDBDataSet)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.studentsBindingSource)).BeginInit();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.studentDBDataSetBindingSource)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.studentDBDataSetBindingSource1)).BeginInit();
             this.SuspendLayout();
             // 
             // button_logout
@@ -70,111 +63,124 @@ namespace StudentDB_Framework
             this.textBox1.TabIndex = 1;
             this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
-            // studentDBDataSet
+            // label1
             // 
-            this.studentDBDataSet.DataSetName = "StudentDBDataSet";
-            this.studentDBDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
-            // 
-            // studentsBindingSource
-            // 
-            this.studentsBindingSource.DataMember = "Students";
-            this.studentsBindingSource.DataSource = this.studentDBDataSet;
-            // 
-            // studentsTableAdapter
-            // 
-            this.studentsTableAdapter.ClearBeforeFill = true;
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(106, 54);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(69, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Class Search";
+            
             // 
             // dataGridView1
             // 
-            this.dataGridView1.AutoGenerateColumns = false;
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
-            this.gwidDataGridViewTextBoxColumn,
-            this.emailDataGridViewTextBoxColumn,
-            this.firstDataGridViewTextBoxColumn,
-            this.lastDataGridViewTextBoxColumn,
-            this.genderDataGridViewTextBoxColumn});
-            this.dataGridView1.DataSource = this.studentsBindingSource;
-            this.dataGridView1.Location = new System.Drawing.Point(91, 107);
+            this.dataGridView1.Location = new System.Drawing.Point(91, 114);
             this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(545, 271);
-            this.dataGridView1.TabIndex = 2;
+            this.dataGridView1.Size = new System.Drawing.Size(540, 264);
+            this.dataGridView1.TabIndex = 4;
             // 
-            // studentDBDataSetBindingSource
+            // label2
             // 
-            this.studentDBDataSetBindingSource.DataSource = this.studentDBDataSet;
-            this.studentDBDataSetBindingSource.Position = 0;
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(461, 39);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(27, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Max";
             // 
-            // studentDBDataSetBindingSource1
+            // label3
             // 
-            this.studentDBDataSetBindingSource1.DataSource = this.studentDBDataSet;
-            this.studentDBDataSetBindingSource1.Position = 0;
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(461, 54);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(24, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Min";
             // 
-            // gwidDataGridViewTextBoxColumn
+            // label4
             // 
-            this.gwidDataGridViewTextBoxColumn.DataPropertyName = "gwid";
-            this.gwidDataGridViewTextBoxColumn.HeaderText = "gwid";
-            this.gwidDataGridViewTextBoxColumn.Name = "gwidDataGridViewTextBoxColumn";
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(461, 73);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(47, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Average";
             // 
-            // emailDataGridViewTextBoxColumn
+            // label5
             // 
-            this.emailDataGridViewTextBoxColumn.DataPropertyName = "email";
-            this.emailDataGridViewTextBoxColumn.HeaderText = "email";
-            this.emailDataGridViewTextBoxColumn.Name = "emailDataGridViewTextBoxColumn";
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(527, 16);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(36, 13);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Grade";
+            
             // 
-            // firstDataGridViewTextBoxColumn
+            // label6
             // 
-            this.firstDataGridViewTextBoxColumn.DataPropertyName = "first";
-            this.firstDataGridViewTextBoxColumn.HeaderText = "first";
-            this.firstDataGridViewTextBoxColumn.Name = "firstDataGridViewTextBoxColumn";
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(527, 39);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(35, 13);
+            this.label6.TabIndex = 9;
+            this.label6.Text = "";
             // 
-            // lastDataGridViewTextBoxColumn
+            // label7
             // 
-            this.lastDataGridViewTextBoxColumn.DataPropertyName = "last";
-            this.lastDataGridViewTextBoxColumn.HeaderText = "last";
-            this.lastDataGridViewTextBoxColumn.Name = "lastDataGridViewTextBoxColumn";
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(527, 54);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(35, 13);
+            this.label7.TabIndex = 10;
+            this.label7.Text = "";
             // 
-            // genderDataGridViewTextBoxColumn
+            // label8
             // 
-            this.genderDataGridViewTextBoxColumn.DataPropertyName = "gender";
-            this.genderDataGridViewTextBoxColumn.HeaderText = "gender";
-            this.genderDataGridViewTextBoxColumn.Name = "genderDataGridViewTextBoxColumn";
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(527, 73);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(35, 13);
+            this.label8.TabIndex = 11;
+            this.label8.Text = "";
             // 
             // Form_main
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(686, 390);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button_logout);
             this.Name = "Form_main";
             this.Text = "Form_main";
             this.Load += new System.EventHandler(this.Form_main_Load_1);
-            ((System.ComponentModel.ISupportInitialize)(this.studentDBDataSet)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.studentsBindingSource)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.studentDBDataSetBindingSource)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.studentDBDataSetBindingSource1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
-
         }
 
         #endregion
 
         private Button button_logout;
         private TextBox textBox1;
-        private StudentDBDataSet studentDBDataSet;
-        private BindingSource studentsBindingSource;
-        private StudentDBDataSetTableAdapters.StudentsTableAdapter studentsTableAdapter;
+        private Label label1;
         private DataGridView dataGridView1;
-        private DataGridViewTextBoxColumn gwidDataGridViewTextBoxColumn;
-        private DataGridViewTextBoxColumn emailDataGridViewTextBoxColumn;
-        private DataGridViewTextBoxColumn firstDataGridViewTextBoxColumn;
-        private DataGridViewTextBoxColumn lastDataGridViewTextBoxColumn;
-        private DataGridViewTextBoxColumn genderDataGridViewTextBoxColumn;
-        private BindingSource studentDBDataSetBindingSource;
-        private BindingSource studentDBDataSetBindingSource1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private Label label5;
+        private Label label6;
+        private Label label7;
+        private Label label8;
     }
 }
--- StudentDB_Framework/StudentDB_Framework/Form_main.cs
+++ StudentDB_Framework/StudentDB_Framework/Form_main.cs
@@ -1,12 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
 using System.Data;
 using System.Data.SqlClient;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace StudentDB_Framework
@@ -31,19 +25,24 @@ namespace StudentDB_Framework
         // Form load event - loads data for the logged-in user
         private void Form_main_Load_1(object sender, EventArgs e)
         {
+            // Clear initial label values
+            label6.Text = "";
+            label7.Text = "";
+            label8.Text = "";
+
             if (!string.IsNullOrEmpty(userEmail))
             {
-                LoadStudentData();
+                LoadStudentGrades();
+                CalculateAndDisplayGradeStats(); // Calculate grade statistics
             }
             else
             {
-                // If no email provided (shouldn't happen in normal flow), show all students
-                this.studentsTableAdapter.Fill(this.studentDBDataSet.Students);
+                MessageBox.Show("No user email provided. Cannot load grades.");
             }
         }
 
         // Load student data for the specific logged-in user
-        private void LoadStudentData()
+        private void LoadStudentGrades()
         {
             try
             {
@@ -52,7 +51,14 @@ namespace StudentDB_Framework
                 using (SqlConnection con = new SqlConnection(connectionString))
                 {
                     con.Open();
-                    string query = "SELECT * FROM Students WHERE email = @email";
+
+                    // Query to get student grades with course names
+                    string query = @"SELECT s.first, s.last, c.name AS course_name, t.grade
+                           FROM Students s
+                           JOIN Taking t ON s.gwid = t.gwid
+                           JOIN Courses c ON t.crn = c.crn
+                           WHERE s.email = @email";
+
                     SqlCommand cmd = new SqlCommand(query, con);
                     cmd.Parameters.AddWithValue("@email", userEmail);
 
@@ -60,14 +66,80 @@ namespace StudentDB_Framework
                     DataTable dt = new DataTable();
                     adapter.Fill(dt);
 
-                    // Unbind the existing source and set the new filtered data
-                    dataGridView1.DataSource = null;
+                    // Display results directly without binding sources
                     dataGridView1.DataSource = dt;
                 }
             }
             catch (Exception ex)
             {
-                
+                MessageBox.Show("Error loading grades: " + ex.Message);
+            }
+        }
+
+        // Calculate and display grade statistics
+        private void CalculateAndDisplayGradeStats()
+        {
+            try
+            {
+                string connectionString = @"Data Source=(LocalDB)\MSSQLLocalDB; AttachDbFilename=|DataDirectory|\StudentDB.mdf;Integrated Security=True";
+
+                using (SqlConnection con = new SqlConnection(connectionString))
+                {
+                    con.Open();
+
+                    // Query to calculate MAX grade
+                    string maxQuery = @"SELECT MAX(CAST(grade AS FLOAT)) 
+                                      FROM Students s
+                                      JOIN Taking t ON s.gwid = t.gwid
+                                      WHERE s.email = @email";
+
+                    SqlCommand maxCmd = new SqlCommand(maxQuery, con);
+                    maxCmd.Parameters.AddWithValue("@email", userEmail);
+                    object maxResult = maxCmd.ExecuteScalar();
+
+                    // Query to calculate MIN grade
+                    string minQuery = @"SELECT MIN(CAST(grade AS FLOAT)) 
+                                      FROM Students s
+                                      JOIN Taking t ON s.gwid = t.gwid
+                                      WHERE s.email = @email";
+
+                    SqlCommand minCmd = new SqlCommand(minQuery, con);
+                    minCmd.Parameters.AddWithValue("@email", userEmail);
+                    object minResult = minCmd.ExecuteScalar();
+
+                    // Query to calculate AVG grade
+                    string avgQuery = @"SELECT AVG(CAST(grade AS FLOAT)) 
+                                      FROM Students s
+                                      JOIN Taking t ON s.gwid = t.gwid
+                                      WHERE s.email = @email";
+
+                    SqlCommand avgCmd = new SqlCommand(avgQuery, con);
+                    avgCmd.Parameters.AddWithValue("@email", userEmail);
+                    object avgResult = avgCmd.ExecuteScalar();
+
+                    // Display results in labels
+                    if (maxResult != DBNull.Value && maxResult != null)
+                        label6.Text = Convert.ToDouble(maxResult).ToString("F2");
+                    else
+                        label6.Text = "N/A";
+
+                    if (minResult != DBNull.Value && minResult != null)
+                        label7.Text = Convert.ToDouble(minResult).ToString("F2");
+                    else
+                        label7.Text = "N/A";
+
+                    if (avgResult != DBNull.Value && avgResult != null)
+                        label8.Text = Convert.ToDouble(avgResult).ToString("F2");
+                    else
+                        label8.Text = "N/A";
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error calculating grade statistics: " + ex.Message);
+                label6.Text = "Error";
+                label7.Text = "Error";
+                label8.Text = "Error";
             }
         }
 
@@ -81,15 +153,57 @@ namespace StudentDB_Framework
                 this.Hide();
 
                 // Create and show the login form
-                Form_login fl = new Form_login(); // Assuming login form is named Form1 now
+                Form_login fl = new Form_login();
                 fl.Show();
             }
         }
 
-       
+        // Search functionality
         private void textBox1_TextChanged(object sender, EventArgs e)
         {
-            
+            // Get the search term from the textbox
+            string searchTerm = textBox1.Text.Trim().ToLower();
+
+            try
+            {
+                // If search box is empty, show all grades
+                if (string.IsNullOrEmpty(searchTerm))
+                {
+                    LoadStudentGrades();
+                    return;
+                }
+
+                string connectionString = @"Data Source=(LocalDB)\MSSQLLocalDB; AttachDbFilename=|DataDirectory|\StudentDB.mdf;Integrated Security=True";
+
+                using (SqlConnection con = new SqlConnection(connectionString))
+                {
+                    con.Open();
+
+                    // Query that filters courses by the search term
+                    string query = @"SELECT s.first, s.last, c.name AS course_name, t.grade
+                                  FROM Students s
+                                  JOIN Taking t ON s.gwid = t.gwid
+                                  JOIN Courses c ON t.crn = c.crn
+                                  WHERE s.email = @email 
+                                  AND (LOWER(c.name) LIKE @search OR CAST(c.crn AS VARCHAR) LIKE @search)";
+
+                    SqlCommand cmd = new SqlCommand(query, con);
+                    cmd.Parameters.AddWithValue("@email", userEmail);
+                    cmd.Parameters.AddWithValue("@search", "%" + searchTerm + "%");
+
+                    SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+                    DataTable dt = new DataTable();
+                    adapter.Fill(dt);
+
+                    // Display filtered results directly
+                    dataGridView1.DataSource = dt;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Search error: " + ex.Message);
+            }
         }
+
     }
 }
